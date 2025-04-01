86fc519e0e3230b4067bbf7d878aa44046b910cc
Kisakye Brendah Prosper <kskbrendah@gmail.com>
2025-04-01 22:59:42 +0300

    prompt

---
+++ workforce_backend/assessment/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+
+class Prompt(models.Model):
+    prompt_text = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.prompt_text
